4f8474b7ebfcafc469406eb516f401e58fe8b63a
NyxoraXv <87947067+NyxoraXv@users.noreply.github.com>
2024-04-12 22:53:56 +0700

    Trial 2

--- KNN TF-IDF Trial 2/cache.py
+++ KNN TF-IDF Trial 2/cache.py
@@ -1,17 +1,13 @@
 import pandas as pd
-import Preprocessor
+import Model
 
 # Comment Sentiment. A value of zero represents a negative sentiment, whereas values of one and two represent neutral
 df = pd.read_csv(r"C:\Users\Folive\Documents\Python\AI\Basic-Machine-Learning\KNN TF-IDF Trial 2\comments.csv")
 sample_df = df.iloc[0:4, :]
-sample_df = sample_df["Comment"]
+xtrain = sample_df["Comment"]
+ytrain = sample_df["Sentiment"]
 
-cleaner = Preprocessor.TextCleaner()
-vectorizer = Preprocessor.TFIDFVectorizer()
+KNN_Model = Model.KNN()
 
-cleaned = cleaner.Clean(sample_df)
-
-vectorizer.fit(cleaned)
-vectorized = vectorizer.Vectorize()
-vectorized = pd.DataFrame(vectorized)
-print(vectorized.values)
+KNN_Model.Fit(xtrain, ytrain)
+KNN_Model.Predict("This is a good pocket")
