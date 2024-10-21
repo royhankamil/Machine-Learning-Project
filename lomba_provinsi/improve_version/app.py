0df8b65929629b7862008f0ead7d8d0b56b3c2de
Achmad Royhan Kamil <royhan1125@gmail.com>
2024-10-21 12:57:03 +0700

    changa

--- lomba_provinsi/improve_version/app.py
+++ lomba_provinsi/improve_version/app.py
@@ -6,17 +6,33 @@ import matplotlib.pyplot as plt
 import pickle
 from algorithm.Algorithm import KNN
 from algorithm.Algorithm import Preprocessing
+import os
 
 # Load pre-trained model and supporting files
 def load_pickle(file_path):
     with open(file_path, 'rb') as file:
         return pickle.load(file)
+    
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, 'model/knn_model.pkl')
 
-model = load_pickle('model/knn_model.pkl')
-word_set = load_pickle('model/word_set.pkl')
-index_dict = load_pickle('model/index_dict.pkl')
-word_count = load_pickle('model/word_count.pkl')
-preprocessing = load_pickle('model/preprocessing.pkl')
+BASE_DIR1 = os.path.dirname(os.path.abspath(__file__))
+model_path1 = os.path.join(BASE_DIR1, 'model/word_set.pkl')
+
+BASE_DIR2 = os.path.dirname(os.path.abspath(__file__))
+model_path2 = os.path.join(BASE_DIR2, 'model/index_dict.pkl')
+
+BASE_DIR3 = os.path.dirname(os.path.abspath(__file__))
+model_path3 = os.path.join(BASE_DIR3, 'model/word_count.pkl')
+
+BASE_DIR4 = os.path.dirname(os.path.abspath(__file__))
+model_path4 = os.path.join(BASE_DIR4, 'model/preprocessing.pkl')
+
+model = load_pickle(model_path)
+word_set = load_pickle(model_path1)
+index_dict = load_pickle(model_path2)
+word_count = load_pickle(model_path3)
+preprocessing = load_pickle(model_path4)
 
 # Function to predict sentiment based on input text
 def predict_sentiment(input_text):
